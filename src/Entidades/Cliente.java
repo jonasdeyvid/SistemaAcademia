a77d2b5f8cb96e7a38115959a6e04357887a08ee
Guilherme Rodrigues <guilherme_rodrigues@alu.ufc.br>
2019-04-03 16:39:44 -0300

    pacotes

---
+++ src/Entidades/Cliente.java
@@ -0,0 +1,15 @@
+package Entidades;
+
+public class Cliente {
+	private String nome;
+	private String endereco;
+	private int contato;
+	private int cpf;
+	
+	public Cliente() {
+		
+	}
+	public Cliente(String nome, String endereco, int contato, int cpf) {
+		
+	}
+}
