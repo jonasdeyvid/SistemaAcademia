0fc05fa0510b4dfff550c9f8807ded1b23f48ca9
Jonas <jonasdeyvid@hotmail.com>
2019-04-03 22:10:06 -0300

    inicio dos testes

---
+++ src/testes/SuiteDeTestes.java
@@ -0,0 +1,22 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuiteDeTestes {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	//testes de cliente
+	public void testaAdicaoDeCliente() {
+		String nome = "joao";
+		String endereco = "Quixada, CE";
+		int contato = 92054322;
+		String cpf = "12345612311";
+	}
+
+}
