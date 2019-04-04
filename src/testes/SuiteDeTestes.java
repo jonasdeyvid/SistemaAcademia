09e368b0a0f5cb2bda6f4d504965d73af4280181
Jonas <jonasdeyvid@hotmail.com>
2019-04-03 22:48:11 -0300

    correcao de libraries

--- src/testes/SuiteDeTestes.java
+++ src/testes/SuiteDeTestes.java
@@ -1,8 +1,11 @@
 package testes;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import Controles.ControladorCliente;
 
 class SuiteDeTestes {
 
@@ -17,6 +20,9 @@ class SuiteDeTestes {
 		String endereco = "Quixada, CE";
 		int contato = 92054322;
 		String cpf = "12345612311";
+		boolean retorno = ControladorCliente.getInstance().addCliente(nome, endereco, contato, cpf);
+		
+		assertEquals(true, retorno);
 	}
 
 }
